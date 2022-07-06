359d51b726caaa1980ed8c445cdc2324987873bf
Brianna Nelson <briannaanelson@gmail.com>
2022-07-05 20:13:03 -0500

    fixed register form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,11 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { Rare } from "./Rare"
 
 export const ApplicationViews = () => {
   return (
-    <h1 >Welcome to Rare Publishing</h1>
+    <>
+    <Route exact path="/" element={<Rare />} />
+    </>
   )
 }
