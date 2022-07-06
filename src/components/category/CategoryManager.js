8a3e992dbce804d8822271506a94f833f1b5b721
Ruby Kaur <rubiblr@gmail.com>
2022-07-05 19:45:08 -0500

    category CRUD

--- src/components/category/CategoryManager.js
+++ src/components/category/CategoryManager.js
@@ -9,8 +9,17 @@ export const getCategory = () => {
         .then(response => response.json())
 }
 
+export const getCategoryById = (id) => {
+    return fetch(`${remoteURL}/category/${id}`, {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
 export const deleteCategory = (id) => {
-    console.log("delete22")
+    console.log(id)
     return fetch(`${remoteURL}/category/${id}`, {
         method: "DELETE",
         headers: {
@@ -19,4 +28,29 @@ export const deleteCategory = (id) => {
      })
         .then(getCategory)
         
+}
+
+export const createCategory = (newcategory) => {
+    return fetch("http://localhost:8000/category", {
+        method: "POST",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newcategory)
+     })
+        .then(getCategory)
+}
+
+export const updateCategory = (category) => {
+    console.log("updatecategory",category)
+    return fetch(`${remoteURL}/category/${category.id}`, {
+        method: "PUT",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(category)
+     })
+        .then(getCategory)
 }
